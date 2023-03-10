b0148e5274f4566d5790a9e7e6949314fda11576
Adam Stolarczyk <115931514+AJSTO@users.noreply.github.com>
2023-03-10 14:23:17 +0100

    delete comments

--- app/main.py
+++ app/main.py
@@ -17,9 +17,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-# in cloudshell:
-# gcloud dataproc jobs submit pyspark --jars gs://spark-lib/bigquery/spark-3.1-bigquery-0.28.0-preview.jar --cluster cluster-adam --region europe-central2 gs://dataproc-staging-europe-central2-305196457839-jazljgsi/notebooks/jupyter/job.py
-
-# in terminal dataproc:
-# spark-submit --jars gs://spark-lib/bigquery/spark-3.1-bigquery-0.28.0-preview.jar home/dataproc/flights_agg/main.py
+    
